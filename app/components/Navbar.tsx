08e12f5bcd063e5346e28fafd15ab44a783f8175
Wellington <w9llington@gmail.com>
2025-08-18 17:49:32 -0300

    resume examples section

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <Link to="/">
+        <p className='text-2xl font-bold text-gradient'>RESUMIND</p>
+      </Link>
+
+      <Link to="/upload" className='primary-button w-fit'>
+        Carregar curriculo
+      </Link>
+    </nav>
+  )
+}
+
+export default Navbar
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,39 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "../../constants/index";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Feedback inteligente para a sua vaga dos sonhos!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('images/bg-main.svg')] md:bg-cover">
+
+    <Navbar />
+
+    <section className="main-section">
+      <h1>Monitore suas vagas e analise seu curriculo</h1>
+      <h2>revise seus envios e receba feedback com uma poderosa IA</h2>
+
+    {
+      resumes.length > 0 && (
+        <div className="resumes-section">
+         {
+           resumes.map((resume: Resume) => (
+             <ResumeCard key={resume.id} resume={resume} />
+            ))
+          }
+        </div>
+      )
+    }
+    </section>
+
+
+  </main>;
 }
